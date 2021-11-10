9d23ff7eee25429d571350cd1c1890f020c3d53f
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-10 17:06:07 +0100

    add result + fix-js-1

--- js/main.js
+++ js/main.js
@@ -5,8 +5,11 @@
 
     - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+
+// Reference
 const containerNum = document.querySelector('.random-numbers');
 const containerTimer = document.querySelector('.timer');
+const result = document.querySelector('.result');
 
 // gen random number
 let randomNum = [];
@@ -19,19 +22,22 @@ console.log(randomNum);
 // timer
 let second = 3;
 let myNumbers = [];
+
 const timer = setInterval( ()=>{
     containerTimer.innerHTML = `<h1>${second}</h1>`
     // rimozione degli elementi nell'html
     if(second === 0){
         clearInterval(timer)
-        containerNum.innerHTML =''
+        containerTimer.innerHTML ='';
+        containerNum.innerHTML ='';
         // inserimento numeri utenti
         let newNum;
         for(let i = 1; i <= randomNum.length; i++){
+            // controllo del numero inseritoo se isNaN
            do{
                newNum = parseInt(prompt(`inserisci il ${i}° Numero:`));
            }while(isNaN(newNum))
-
+        // creazione dell'array con i numeri scelti dall'utente
            myNumbers.push(newNum);
         }
         // creazione array elementi uguali tra randomNum & myNumbers
@@ -42,6 +48,25 @@ const timer = setInterval( ()=>{
         });
         console.log(myNumbers);
         console.log(pari);
+        console.log(`hai indovinato ${pari.length} numeri`);
+
+        // risultato
+        pari.forEach((el)=>{
+            containerNum.innerHTML +=`
+            <h2>${el}</h2>`;
+        });
+        // risultato visto dall'utente
+        let resultText = '';
+        let h2 = document.createElement('h2');
+        
+        if(pari.length === randomNum.length){
+            resultText = `hai indovinato ${pari.length} numeri: HAI VINTO!`;
+        }else {
+            resultText = `hai indovinato solo ${pari.length} numeri: RITENTA `;
+        }
+        h2.append(resultText);
+        result.append(h2);
+        
     } else{
         second--
     }
@@ -52,5 +77,5 @@ const timer = setInterval( ()=>{
    F U N Z I O N I
 ********************/
 function randomNumber(){
-   return Math.floor(Math.random() * 999) + 1;
+   return Math.floor(Math.random() * 99) + 1;
 }
